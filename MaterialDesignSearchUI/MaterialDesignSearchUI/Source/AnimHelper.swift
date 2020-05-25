cc15a34e2a63de0f53581d1aff466607e55aa340
Michael Ho <twho@umich.edu>
2020-05-24 18:12:26 -0700

    Fix animation bug

--- MaterialDesignSearchUI/MaterialDesignSearchUI/Source/AnimHelper.swift
+++ MaterialDesignSearchUI/MaterialDesignSearchUI/Source/AnimHelper.swift
@@ -14,25 +14,26 @@ class AnimHelper {
     /**
      Move up the views to specified bottom position.
      
-     - Parameter view: The view to be applied this moving animation.
-     - Parameter duration: The duration of the animation.
+     - Parameter views:      The views to be applied this moving animation.
+     - Parameter duration:   The duration of the animation.
      - Parameter completion: The completion handler.
-     - Parameter fromY: The origin y coordinate of the views.
+     - Parameter show:       The boolean to determine move in or out views. Set true to move in.
      */
-    public func moveUpViews(_ views: [UIView], duration: TimeInterval = 0.5, completion: ((Bool) -> Void)? = nil, fromY: CGFloat? = nil, show: Bool) {
+    public func moveUpViews(_ views: [UIView], duration: TimeInterval = 0.5, completion: ((Bool) -> Void)? = nil, show: Bool) {
         views.forEach({
-            let originY = fromY == nil ? $0.frame.maxY : fromY!
             $0.alpha = show ? 0.0 : 1.0
-            $0.transform = CGAffineTransform(translationX: 0, y: originY)
+            if show {
+                $0.transform = CGAffineTransform(translationX: 0, y: $0.frame.maxY)
+            }
         })
         UIView.animate(withDuration: duration,
                        delay: 0,
                        usingSpringWithDamping: 0.9,
                        initialSpringVelocity: 0.15,
-                       options: .curveEaseOut,
+                       options: .curveEaseInOut,
                        animations: { () -> Void in
                         views.forEach {
-                            $0.transform = CGAffineTransform.identity
+                            $0.transform = show ? .identity : CGAffineTransform(translationX: 0, y: -$0.frame.maxY)
                             $0.alpha = show ? 1.0 : 0.0
                         }
         }, completion: completion)
@@ -40,26 +41,26 @@ class AnimHelper {
     /**
      Move down the views from specified top position to its right position.
      
-     - Parameter view: The view to be applied this moving animation.
-     - Parameter duration: The duration of the animation.
+     - Parameter views:      The views to be applied this moving animation.
+     - Parameter duration:   The duration of the animation.
      - Parameter completion: The completion handler.
-     - Parameter toY: The y coordinate of the views as the destination position.
-     - Parameter show: The boolean to determine move in or out views. Set true to move in.
+     - Parameter show:       The boolean to determine move in or out views. Set true to move in.
      */
-    public func moveDownViews(_ views: [UIView], duration: TimeInterval = 0.3, completion: ((Bool) -> Void)? = nil, toY: CGFloat? = nil, show: Bool) {
+    public func moveDownViews(_ views: [UIView], duration: TimeInterval = 0.5, completion: ((Bool) -> Void)? = nil, show: Bool) {
         views.forEach({
             $0.alpha = show ? 0.0 : 1.0
-            $0.transform = show ? CGAffineTransform(translationX: 0, y: -$0.frame.height) : .identity
+            if show {
+                $0.transform = CGAffineTransform(translationX: 0, y: -$0.frame.maxY)
+            }
         })
         UIView.animate(withDuration: duration,
                        delay: 0,
                        usingSpringWithDamping: 0.9,
                        initialSpringVelocity: 0.15,
-                       options: .curveEaseOut,
+                       options: .curveEaseInOut,
                        animations: { () -> Void in
                         views.forEach {
-                            let destY = toY == nil ? $0.frame.height : toY!
-                            $0.transform = show ? .identity : CGAffineTransform(translationX: 0, y: destY)
+                            $0.transform = show ? .identity : CGAffineTransform(translationX: 0, y: $0.frame.maxY)
                             $0.alpha = show ? 1.0 : 0.0
                         }
         }, completion: completion)
